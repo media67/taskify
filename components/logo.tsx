e76552ee4e13c4ba679313dac802af44510d609b
Samuel Bourassa <district67@gmail.com>
2023-11-23 01:24:39 -0500

    List compopnent, List Header.

--- components/logo.tsx
+++ components/logo.tsx
@@ -17,9 +17,10 @@ export const Logo = () => {
           alt="Logo"
           height={30}
           width={30}
+          className="pb-1"
         />
         <p className={cn(
-          "text-lg text-neutral-700 pb-1",
+          "text-lg text-neutral-700",
           headingFont.className,
         )}>
           Taskify
