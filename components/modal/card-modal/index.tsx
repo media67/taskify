ea580e868061a9ffca066554e6b742fbb46da4e1
Samuel Bourassa <district67@gmail.com>
2023-11-24 05:28:08 -0500

    Card actions.

--- components/modal/card-modal/index.tsx
+++ components/modal/card-modal/index.tsx
@@ -2,12 +2,14 @@
 
 import { useQuery } from "@tanstack/react-query";
 
-import { Dialog, DialogContent } from "@/components/ui/dialog";
-import { useCardModal } from "@/hooks/use-card-modal";
 import { CardWithList } from "@/types";
 import { fetcher } from "@/lib/fetcher";
+import { useCardModal } from "@/hooks/use-card-modal";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 
 import { Header } from "./header";
+import { Description } from "./description";
+import { Actions } from "./actions";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -20,9 +22,29 @@ export const CardModal = () => {
   });
 
   return (
-    <Dialog open={isOpen} onOpenChange={onClose}>
+    <Dialog
+      open={isOpen}
+      onOpenChange={onClose}
+    >
       <DialogContent>
-        {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+        {!cardData
+          ? <Header.Skeleton />
+          : <Header data={cardData} />
+        }
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {!cardData
+                ? <Description.Skeleton />
+                : <Description data={cardData} />
+              }
+            </div>
+          </div>
+          {!cardData
+            ? <Actions.Skeleton />
+            : <Actions data={cardData} />
+          }
+        </div>
       </DialogContent>
     </Dialog>
   );
